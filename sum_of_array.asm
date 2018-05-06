de8ce7dd5cc43f5ffe5d7f767b960201b808a5ce
“MasfiqurRahaman” <masfiqallid@gmail.com>
2018-05-06 21:39:21 +0600

    Initial commit

---
+++ sum_of_array.asm
@@ -0,0 +1,33 @@
+;INPUT: A NUMBER N
+; OUTUT: PRINT N-1 TO 1
+
+.MODEL SMALL
+.DATA 
+MSG DB 'abcde'
+W DW 10,20,30,40,50,60
+GAMMA DW 100 DUP(10) 
+DELTA DW 100 DUP(?)
+.CODE
+
+MAIN PROC
+ 
+    MOV AX, @DATA
+    MOV DS, AX  
+    CALL TESTER
+MAIN ENDP   
+    TESTER PROC
+    
+    XOR AX, AX
+    MOV CX, 6
+    LEA SI, W
+    TOP:
+    ADD AX, [si]
+    ADD SI, 2
+    
+    LOOP TOP 
+    
+        
+    TESTER ENDP
+
+
+END MAIN
